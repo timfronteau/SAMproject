3e1a4d157b89c88ab7c3bc96ff290c84a5254e64
vsinic <86923381+vsinic@users.noreply.github.com>
2022-01-21 16:35:30 +0100

    simplify baselineFusion

--- baselineFusion.py
+++ baselineFusion.py
@@ -19,7 +19,31 @@ class BaselineFusion(Baseline):
         self.nb_clf = len(self.DATA_TYPE_LIST) # number of classifiers`
         self.model_name = 'fusion_model'
 
-    def build_model(self):
+    def build_model(self): 
+        clfs = self.__build_sub_model()
+        #inputs = [Input(shape=(1, )) for _ in self.nb_clf]    
+        embeddings1 = [Dense(self.nb_of_label, activation='relu', name=f'fusion_embeddings_1_{idx}')(clf.layers[-1].output) for idx,clf in enumerate(clfs)]
+        embeddings2 = [Dense(self.nb_of_label, activation='relu', name=f'fusion_embeddings_2_{idx}')(emb) for idx,emb in enumerate(embeddings1)]
+        merged = Concatenate(axis=1, name='fusion_concat')(embeddings2)
+        
+        dense1 = Dense(self.nb_clf * self.nb_of_label, activation='relu', name='fusion_dense_1')(merged)
+        dropout1 = Dropout(0.05, name='fusion_dropout_1')(dense1)
+        dense2 = Dense(self.nb_clf * self.nb_of_label, activation='relu', name='fusion_dense_2')(dropout1)
+        dropout2 = Dropout(0.05, name='fusion_dropout_2')(dense2)
+        dense3 = Dense(32, activation = 'relu', name='fusion_dense_3')(dropout2)
+        dropout3 = Dropout(0.05, name='fusion_dropout_3')(dense3)
+        output = Dense(self.nb_of_label, activation='softmax', name='fusion_output')(dropout3)
+
+        inputs = [m.layers[0].input for m in clfs]
+        m = Model(inputs=inputs, outputs=output, name=self.model_name)
+        print(m.summary())
+        m.compile(optimizer='adam',loss='categorical_crossentropy', metrics=['accuracy']) # , optimizer=opt
+        plot_model(model=m, to_file=f'{self.model_name}_tree.png', show_shapes=True)
+        self.model = m
+
+    def __build_sub_model(self):
+        """"
+        Cette methode permet de creer de récupérer les classifieurs unimodaux"""
         clfs = []
         for DATA_TYPE in self.DATA_TYPE_LIST :
             if DATA_TYPE=='Image':
@@ -51,37 +75,13 @@ class BaselineFusion(Baseline):
             y_val = dataset_baseline['y_val']
             y_test = dataset_baseline['y_test']
 
-            BATCH_SIZE = 32
-            EPOCHS = 10
-
             baseline = MODEL_CLASS(X_train, X_val, X_test, y_train, y_val, y_test,
-                            self.nb_of_label, batch_size=BATCH_SIZE, epochs=EPOCHS, input_shape=INPUT_SHAPE)
+                            self.nb_of_label, input_shape=INPUT_SHAPE)
             baseline.load_model(MODEL_DIR)
             m = Model(baseline.model.inputs, baseline.model.output, name=f'{MODEL_DIR}')
             m.trainable = False
-            clfs.append(m)
             print(m.summary())
-
-        #inputs = [Input(shape=(1, )) for _ in self.nb_clf]    
-        embeddings1 = [Dense(self.nb_of_label, activation='relu', name=f'fusion_embeddings_1_{idx}')(clf.layers[-1].output) for idx,clf in enumerate(clfs)]
-        embeddings2 = [Dense(self.nb_of_label, activation='relu', name=f'fusion_embeddings_2_{idx}')(emb) for idx,emb in enumerate(embeddings1)]
-        merged = Concatenate(axis=1, name='fusion_concat')(embeddings2)
-        
-        dense1 = Dense(self.nb_clf * self.nb_of_label, activation='relu', name='fusion_dense_1')(merged)
-        dropout1 = Dropout(0.05, name='fusion_dropout_1')(dense1)
-        dense2 = Dense(self.nb_clf * self.nb_of_label, activation='relu', name='fusion_dense_2')(dropout1)
-        dropout2 = Dropout(0.05, name='fusion_dropout_2')(dense2)
-        dense3 = Dense(32, activation = 'relu', name='fusion_dense_3')(dropout2)
-        dropout3 = Dropout(0.05, name='fusion_dropout_3')(dense3)
-        output = Dense(self.nb_of_label, activation='softmax', name='fusion_output')(dropout3)
-
-        inputs = [m.layers[0].input for m in clfs]
-        m = Model(inputs=inputs, outputs=output, name=self.model_name)
-        print(m.summary())
-        m.compile(optimizer='adam',loss='categorical_crossentropy', metrics=['accuracy']) # , optimizer=opt
-        plot_model(model=m, to_file=f'{self.model_name}_tree.png', show_shapes=True)
-        self.model = m
-
-
+            clfs.append(m)
+        return clfs
 
       
